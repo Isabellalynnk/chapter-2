492b3c19d3ab15e080927a5a047d354a8bc10a2a
Isabellalynnk <49414516+Isabellalynnk@users.noreply.github.com>
2019-11-14 15:17:53 -0600

    Initial commit

---
+++ chapter-2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chapter_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            int num2 = 15;
+            int resultAdd;
+            int resultMinus;
+            int resultDivide;
+
+        }
+    }
+}
